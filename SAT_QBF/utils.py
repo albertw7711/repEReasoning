84c1126d42818358c669ab79a486a877f03d7b9f
Albert W <albertw7711@tutanota.com>
2025-03-01 21:55:24 -0500

    updated sym_generator.cpython-312.pyc, sym_verifier.py and 2 others

---
+++ SAT_QBF/utils.py
@@ -0,0 +1,4 @@
+def is_trivial(cnf):
+    if not cnf:
+        return True  # empty CNF: trivially satisfiable
+
--- symbolic_differentiation/v2/sym_verifier.py
+++ symbolic_differentiation/v2/sym_verifier.py
@@ -20,3 +20,32 @@ class SymDifferentiationVerifier:
     def get_sol_for_node_helper(self, node, queue, visited):
         step = None
         if node.is_solved:
+            if node.expr not in visited:
+                step = Step(9, node.expr, node.expr, None, node.deri)
+                queue.append(step)
+                visited.add(node.expr)
+                return
+        else:
+            match node.rule:
+                case None:
+                    step = Step(node.rule, node.expr, None, None, node.deri)
+                case 0|1|2:
+                    step = Step(node.rule, node.expr, node.p1.expr, node.p2.expr)
+            queue.append(step)
+            node.is_solved = True
+            visited.add(node.expr)
+        if node.rule is not None:
+            self.get_sol_for_node_helper(node.p1, queue, visited)
+            self.get_sol_for_node_helper(node.p2, queue, visited)
+
+    def print_sol(self, full_sol):
+        for step in full_sol:
+            print(step.rule, " | ", step.target, " | ", step.u, " | ", step.v, " | ", step.sol)
+
+if __name__ == "__main__":
+    generator = SymDifferentiationGenerator(product_max=3, chain_max=3, section_length=5,
+                                            is_poly=true, is_trig=true, is_exp=true)
+    generator.generate_curriculum()
+    verifier = SymDifferentiationVerifier()
+    full_sol = verifier.get_sol_for_node(generator.output_db[5][0])
+    verifier.print_sol(full_sol)
